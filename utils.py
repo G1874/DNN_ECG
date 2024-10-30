d397de7bd2988207e0a21c2851d54b34087855cf
Grzegorz Niedziółka <grzegorz@niedziolka.pl>
2024-10-30 18:26:29 +0100

    Created ModelTrainer class

---
+++ utils.py
@@ -0,0 +1,16 @@
+from torch.utils.data import DataLoader
+import torch
+
+class ModelTrainer():
+    def __init__(self):
+        pass
+
+    def train_model(self, train_loader:DataLoader, loss_fn,
+                    optimizer, device:torch.device, epchos:int):
+        pass
+
+    def train_one_epoch(self, ):
+        pass
+
+    def validate_model():
+        pass
