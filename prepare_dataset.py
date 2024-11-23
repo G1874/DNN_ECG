e8086cf67f7762602200e27de359e5dff458e2e3
Grzegorz Niedziółka <grzegorz@niedziolka.pl>
2024-11-23 17:07:10 +0100

    prepare_dataset.py script

---
+++ prepare_dataset.py
@@ -0,0 +1,16 @@
+from preprocessing import EcgDatasetCompiler
+
+#
+# Prepare dataset for training.
+# For now works only for mit_bih_atrial_fibrillation_database.
+#
+
+ecgDatasetCompiler = EcgDatasetCompiler(
+    "Data/train_dataset",   # Output path of the compiled dataset.
+    250,                    # Sampling rate in Hz.
+    5*250,                  # Size of a single slice of the record.
+    0.5                     # Threshold of afib ratio in a slice to classify it as afib
+)
+
+ecgDatasetCompiler.compileEcgDataset("./Data/mit_bih_atrial_fibrillation_database/files")
+ecgDatasetCompiler.restructureDataset(deleteFiles=True)
