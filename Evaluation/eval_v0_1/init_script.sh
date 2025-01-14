4506124f540e4f2a5ea4d8ef90ae30afce7c8d8f
Grzegorz Niedziółka <grzegorz@niedziolka.pl>
2025-01-14 19:04:11 +0100

    Added checkpoints to model training

--- Evaluation/eval_v0_1/init_script.sh
+++ Evaluation/eval_v0_1/init_script.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 conda install python=3.10
 conda install numpy=1.24.1
 pip install -r requirements.txt
--- Utils/model_trainer.py
+++ Utils/model_trainer.py
@@ -1,9 +1,12 @@
 from torch.utils.data import DataLoader
 import torch
+from datetime import datetime
+import os
+
 
 class ModelTrainer():
-    def __init__(self):
-        pass
+    def __init__(self, save_model_path=None):
+        self.save_model_path = save_model_path
 
     def train_model(self, net, train_loader:DataLoader, val_loader:DataLoader,
                     loss_fn, optimizer, device:torch.device, epchos:int, scheduler=None):
@@ -12,6 +15,8 @@ class ModelTrainer():
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.device = device
+
+        best_vloss = 0
 
         for epoch in range(epchos):
             print(f'EPOCH {epoch + 1}:')
@@ -27,6 +32,13 @@ class ModelTrainer():
 
             if scheduler:
                 scheduler.step()
+
+            # Track best performance, and save the model's state
+            if (avg_vloss < best_vloss) and (self.save_model_path):
+                best_vloss = avg_vloss
+                timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+                model_path = f"{self.save_model_path}-{timestamp}.pt"
+                torch.save(self.net.state_dict(), model_path)
 
             print(f'LOSS train {avg_loss} valid {avg_vloss} ACCURACY {vaccuracy}')
 
