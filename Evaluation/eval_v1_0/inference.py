cb44da69b200174902cf564f603449d4b16b6963
Grzegorz Niedziółka <grzegorz@niedziolka.pl>
2025-01-19 19:16:28 +0100

    afib_detector_v4_0.py

---
+++ Evaluation/eval_v1_0/inference.py
@@ -0,0 +1,105 @@
+import numpy as np
+import torch
+from torchvision import transforms
+from wfdb import processing
+from preprocessing import ToSpectrogram, ToTensor, BandPassFilter
+from afib_detector_v4_0 import AfibDetector
+
+
+class AfibInference():
+    def __init__(self):
+        self.model_path = "./afib_detector_v4_1_1-20250117_221903-2.pt"
+        self.fs = 125 # Sampling frequency
+        self.inference_window = 5 * self.fs
+        self.stride = 5 * self.fs
+
+        filter_config = {
+            "lowcut": 0.5,
+            "highcut": 50,
+            "fs": self.fs,
+            "order": 5
+        }
+        self.filter = BandPassFilter(filter_config)
+
+        sptectrogram_config = {
+            "window_size": 128,
+            "stride": 128//8,
+            "fs": self.fs
+        }
+
+        self.transform = transforms.Compose([
+            ToSpectrogram(sptectrogram_config),
+            ToTensor(),
+            transforms.Resize([128, 128])
+            # transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+
+    def preprocessSignal(self, signal, fs):
+        signal = signal[:,0]
+        
+        if fs != self.fs:
+            signal, _ = processing.resample_sig(
+                signal,
+                fs,             # Original frequency
+                self.fs         # Frequency target
+            )
+
+        signal = processing.normalize_bound(signal, -1, 1)
+        
+        if self.filter is not None:
+            signal = self.filter(signal)
+
+        return signal
+    
+    def makeInference(self, signal, fs):
+        length = signal.shape[0]
+
+        output_mask = np.zeros(length, dtype=np.float32)
+        overlap = np.zeros(length, dtype=np.float32)
+
+        net = self.loadNetwork()
+
+        for step in range(0, length//self.stride):
+            idx = step * self.stride
+
+            if (idx + self.inference_window) > length:
+                idx = length - self.inference_window
+
+            overlap[idx:(idx+self.inference_window)] += 1.0
+            slice = signal[idx:(idx+self.inference_window)]
+            
+            prediction = self.classifySlice(net, slice)
+
+            output_vector = np.ones(self.inference_window) * prediction.numpy()
+            p_output_vector = output_mask[idx:(idx+self.inference_window)]
+            overlap_vector = overlap[idx:(idx+self.inference_window)]
+
+            output_mask[idx:(idx+self.inference_window)] = ((overlap_vector - 1) * p_output_vector + output_vector) / overlap_vector
+
+        if fs != self.fs:
+            output_mask, _ = processing.resample_sig(
+                np.where(output_mask >= 0.5, 1.0, 0.0),
+                self.fs,        # Original frequency
+                fs              # Frequency target
+            )
+
+        return np.where(output_mask >= 0.5, 1.0, 0.0)
+
+    def classifySlice(self, net, slice):
+        input = self.transform(slice)
+        input = input.reshape((1,input.shape[0],input.shape[1],input.shape[2]))
+
+        with torch.no_grad():
+            output = net(input)
+        prediction = torch.argmax(output)
+
+        return prediction
+
+    def loadNetwork(self):
+        net = AfibDetector()
+        net.load_state_dict(torch.load(self.model_path,
+                                       weights_only=True,
+                                       map_location=torch.device('cpu')))
+        net.eval()
+
+        return net
---
+++ Evaluation/eval_v1_0/preprocessing.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import ShortTimeFFT
+from scipy.signal.windows import hamming
+from scipy.signal import butter, filtfilt
+import torch
+
+
+class ToSpectrogram():
+    def __init__(self, configuration: dict):
+        self.w_N = configuration["window_size"]
+        self.hop = configuration["stride"]
+        self.fs = configuration["fs"]
+
+    def __call__(self, sample):
+        w = hamming(self.w_N, True)
+        STFT = ShortTimeFFT(w, self.hop, self.fs)
+        Sx2 = STFT.spectrogram(sample)
+        # Sx_dB = 10 * np.log10(np.clip(Sx2, 1e-4, 1e4))
+        Sx_dB = 10 * np.log10(Sx2)
+        Sx_dB = Sx_dB[:,(-STFT.p_min):(STFT.p_max(sample.shape[0]))]
+        Sx_dB = np.flip(Sx_dB, axis=0)
+
+        return Sx_dB.copy()
+
+
+class ToTensor():
+    def __call__(self, sample):
+        min_val = sample.min()
+        max_val = sample.max()
+        sample = (sample - min_val) / (max_val - min_val + 1e-8)
+
+        sample = torch.tensor(sample.astype(np.float32))
+        return sample.reshape((1,sample.shape[0],sample.shape[1]))
+
+
+class BandPassFilter():
+    def __init__(self, configuration: dict):
+        self.lowcut = configuration["lowcut"]
+        self.highcut = configuration["highcut"]
+        self.fs = configuration["fs"]
+        self.order = configuration["order"]
+        
+        self.b, self.a = butter(self.order, [self.lowcut / (0.5 * self.fs), self.highcut / (0.5 * self.fs)], btype='band')
+
+    def __call__(self, signal):
+        return filtfilt(self.b, self.a, signal)
